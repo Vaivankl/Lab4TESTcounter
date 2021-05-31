542983e1082fc0ae6cd7ce6af968df7bf3195890
Vaivankl <vaiva.niuklyte@knf.stud.vu.lt>
2021-05-31 19:16:39 +0300

    Final changes

--- app/src/main/java/com/example/textcountersample/utils/ElementsCalculator.java
+++ app/src/main/java/com/example/textcountersample/utils/ElementsCalculator.java
@@ -1,25 +1,29 @@
 package com.example.textcountersample.utils;
 
 public class ElementsCalculator {
-    public static int getCharsCount(String inputText) {
-        if (inputText != null) {
-            return inputText.length();
+    public static int getCharsCount(String Text) {
+        if (Text != null) {
+            return Text.length();
+
         }
         return 0;
     }
 
-    public static int getWordsCount(String userInputText) {
+    public static int getWordsCount(String TextWords) {
         int count = 0;
-        char[] symbolsArray = userInputText.toCharArray();
-        for (char c : userInputText.toCharArray()) {
+
+        for (char c : TextWords.toCharArray()) {
+
             if (Character.isSpaceChar(c)) {
                 count++;
             }
         }
+
         if (count == 0) {
             return 1;
         } else {
             return count;
         }
     }
+
 }
