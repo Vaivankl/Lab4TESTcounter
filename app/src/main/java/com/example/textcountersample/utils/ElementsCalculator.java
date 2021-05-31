6f9b7206f516b358bce7a4abcfdc12e881122e16
Vaivankl <vaiva.niuklyte@knf.stud.vu.lt>
2021-05-31 19:00:23 +0300

    Test Added

--- app/src/main/java/com/example/textcountersample/utils/ElementsCalculator.java
+++ app/src/main/java/com/example/textcountersample/utils/ElementsCalculator.java
@@ -1,7 +1,25 @@
 package com.example.textcountersample.utils;
 
 public class ElementsCalculator {
-    public static int getCharsCount(String inputText){
-        return inputText.length();
+    public static int getCharsCount(String inputText) {
+        if (inputText != null) {
+            return inputText.length();
+        }
+        return 0;
+    }
+
+    public static int getWordsCount(String userInputText) {
+        int count = 0;
+        char[] symbolsArray = userInputText.toCharArray();
+        for (char c : userInputText.toCharArray()) {
+            if (Character.isSpaceChar(c)) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return 1;
+        } else {
+            return count;
+        }
     }
 }
--- app/src/test/java/com/example/textcountersample/ExampleUnitTest.java
+++ app/src/test/java/com/example/textcountersample/ExampleUnitTest.java
@@ -1,17 +1,118 @@
 package com.example.textcountersample;
 
+import com.example.textcountersample.utils.ElementsCalculator;
+
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
+public class  ElementsCalculatorUnitTests {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Given_Null_Return_NullPointerException(){
+        final String givenString = null;
+
+        final int expectedResult = 0;
+        final int actualResult = ElementsCalculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
     }
+    @Test
+    public void Given_Null_Return_NullPointerException(){
+        final String givenString = null;
+
+        final int expectedResult = 0;
+        final int actualResult = ElementsCalculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+
+    @Test
+    public void Given_NoSpaces_Return_NumberOfCharsInString(){
+        final String givenString = "labas";
+
+        final int expectedResult = 5;
+        final int actualResult = ElementsCalculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void Given_NoSpaces_Return_NumberOfCharsInString(){
+        final String givenString = "labas";
+
+        final int expectedResult = 5;
+        final int actualResult = ElementsCalculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void Given_StringWithSpacesOnly_Return_dZero(){
+        final String givenString = "   ";
+
+        final int expectedResult = 0;
+        final int actualResult = ElementsCalculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void Given_StringWithSpacesOnly_Return_dZero(){
+        final String givenString = "   ";
+
+        final int expectedResult = 0;
+        final int actualResult = ElementsCalculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+
+    @Test
+    public void Given_SymbolsOnly_Return_NumberOfSymbols(){
+        final String givenString = ".,;:(){}";
+
+        final int expectedResult = 8;
+        final int actualResult = ElementsCalculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void Given_SymbolsOnly_Return_NumberOfSymbols(){
+        final String givenString = ".,;:(){}";
+
+        final int expectedResult = 8;
+        final int actualResult = ElementsCalculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+
+
+    @Test
+    public void Given_hWordsSeparatedWithSpaces_Return_NumberOfNonWhitespaceCharacters(){
+        final String givenString = "a b c";
+
+        final int expectedResult = 3;
+        final int actualResult = ElementsCalculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void Given_SymbolsOnly_Return_NumberOfSymbols(){
+        final String givenString = ".,;:(){}";
+
+        final int expectedResult = 8;
+        final int actualResult = ElementsCalculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
